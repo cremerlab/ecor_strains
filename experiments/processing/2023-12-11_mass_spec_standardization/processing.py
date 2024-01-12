0fd59a55322200f2ec511ba62ee793b86ed326e2
gchure <griffinchure@gmail.com>
2024-01-12 15:51:00 -0800

    Working on mapping mass spec data to reference

---
+++ experiments/processing/2023-12-11_mass_spec_standardization/processing.py
@@ -0,0 +1,57 @@
+#%%
+import numpy as np 
+import pandas as pd 
+
+standard = pd.read_csv('../../../data/compiled_mass_fractions.csv')
+standard = standard[(standard['strain']=='NCM3722') & 
+                    (standard['growth_rate_hr'] >= 0.9) & 
+                    (standard['growth_rate_hr'] <= 0.95)]
+
+#%%
+ref_cond = standard.groupby(['gene_name', 'b_number', 'cog_class', 'cog_letter'])['mass_frac'].agg(('mean')).reset_index()
+ref_cond
+
+#%%
+data = pd.read_csv('../../../data/relative_protein_abundances.csv')
+
+#%% 
+# data['gn_lower'] = data['Gene names'].str.lower()
+data['ref_mass_frac'] = None
+data['gene_name'] = None
+data['b_nubmer'] = None
+data['cog_letter'] = None
+data['cog_class'] = None
+names = data['Gene names'].str.lower()
+cols = [k for k in data.keys() if ('log2' not in k) & ('Relative_concentration' in k)]
+ref_col = data['Relative_concentration_NCM_Glu'].values
+mapped = pd.DataFrame([])
+err = 0
+for g, d in ref_cond.groupby(['gene_name', 'b_number', 'cog_class', 'cog_letter']):
+      ind = None
+      for i, n in enumerate(names):
+            if g[0].lower() in n:
+                ind = i
+                break
+            elif g[1].lower() in n:
+                 ind = i 
+                 break 
+      if ind is not None:
+        _df = data.iloc[ind]
+        for c in cols:
+            cond = c.split('_')[-1]
+            if cond == 'Ace':
+                cond = 'acetate'
+            else:
+                cond = 'glucose'
+            df = pd.DataFrame({'gene_name': g[0],
+                               'b_number': g[1],
+                               'cog_letter': g[3],
+                               'cog_class': g[2],
+                               'relative_conc': _df[c] / ref_col[ind],
+                               'ref_mass_frac': d['mass_frac'].values[0],
+                               'strain': c.split('_')[2],
+                               'carbon_source': cond}, index =[0]) 
+            mapped = pd.concat([mapped, df], sort=False)
+#%%
+mapped['mass_frac'] = mapped['relative_conc'] * mapped['ref_mass_frac']
+mapped.to_csv('../../../data/mapped_relative_protein_abundances.csv', index=False)
